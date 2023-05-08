2702205e98d2d522cb1856099d89e95ce2a33cc8
thakyZ <950594+thakyZ@users.noreply.github.com>
2023-05-08 14:16:35 -0700

    Add changes

---
+++ UpdateWorkspace.ps1
@@ -0,0 +1,51 @@
+[CmdletBinding()]
+param()
+
+class ProjectPath {
+  [string]$path
+}
+
+If ($PSBoundParameters.ContainsKey("Debug")) {
+  $DebugPreference = "Continue"
+}
+
+$WorkspaceFile = (Join-Path -Path $PSScriptRoot -ChildPath "FinalFantasyXIV.code-workspace");
+$BaseDirectory = (Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath ..));
+
+If (-not (Test-Path -LiteralPath $WorkspaceFile -PathType Leaf)) {
+  Write-Error "Workspace file does not exist at: $WorkspaceFile";
+}
+Else {
+  Write-Debug -Message "`$WorkspaceFile: $WorkspaceFile";
+  $WorkspaceFile = (Get-Item -LiteralPath $WorkspaceFile);
+}
+
+If (-not (Test-Path -LiteralPath $BaseDirectory -PathType Container)) {
+  Write-Error "Could not find base directory at: $BaseDirectory";
+}
+Else {
+  Write-Debug -Message "`$BaseDirectory: $BaseDirectory";
+  $BaseDirectory = (Get-Item -LiteralPath $BaseDirectory);
+}
+
+$WorkspaceJson = (Get-Content -LiteralPath $WorkspaceFile.FullName | ConvertFrom-Json -Depth 100);
+
+$WorkspaceJson.folders = @();
+
+$ProjectFolders = (Get-ChildItem -LiteralPath $BaseDirectory -Recurse -Depth 0 -Directory);
+
+$ProjectFoldersArray = @();
+
+ForEach ($Folder in $ProjectFolders) {
+  $RelativePath = (Resolve-Path -LiteralPath $Folder.Fullname -Relative) -Replace "`\`\", "/";
+  Write-Debug -Message "`$RelativePath: $RelativePath";
+  $NewObject = (New-Object -TypeName ProjectPath);
+  $NewObject.path = $RelativePath
+  $ProjectFoldersArray += $NewObject;
+}
+
+$WorkspaceJson.folders = $ProjectFoldersArray;
+
+$NewJson = ($WorkspaceJson | ConvertTo-Json -Depth 100);
+
+$NewJson | Set-Content -LiteralPath $WorkspaceFile.Fullname;
